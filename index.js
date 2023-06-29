066e5ecb3773902fd73acd8f7cf3625ccc4185a8
alicesin615 <alicesin0615@gmail.com>
2023-06-30 00:05:00 +0800

    set default font

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Text } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.style = { fontFamily: 'Roboto' };
 
 AppRegistry.registerComponent(appName, () => App);
