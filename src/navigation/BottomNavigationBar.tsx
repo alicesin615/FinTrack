37f47dbd24d55d9269320482322e2461de98e1f3
alicesin615 <alicesin0615@gmail.com>
2023-07-06 00:02:29 +0800

    simple bottom nav bar UI

--- src/navigation/BottomNavigationBar.tsx
+++ src/navigation/BottomNavigationBar.tsx
@@ -1,25 +1,101 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home, Transactions } from '@screens/index';
-
-import styled, { useTheme } from 'styled-components/native';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import { useTheme } from 'styled-components/native';
+import { GestureResponderEvent, TouchableOpacity, View } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 interface TabScreenProps {
-    name: string;
-    component: any;
+    children?: React.ReactNode;
+    onPress?: ((event: GestureResponderEvent) => void) | undefined;
 }
-const TabScreen = ({ name, component }: TabScreenProps) => {
-    <Tab.Screen name={name} component={component} />;
+const PostNewRecordTab = ({ children, onPress }: TabScreenProps) => {
+    const theme = useTheme();
+
+    return (
+        <TouchableOpacity
+            style={{
+                top: -24,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <View
+                style={{
+                    width: 48,
+                    height: 48,
+                    borderRadius: 48,
+                    backgroundColor: theme.colors.primary
+                }}
+            >
+                {children}
+            </View>
+        </TouchableOpacity>
+    );
 };
 export function BottomNavigationBar() {
     const theme = useTheme();
 
     return (
         <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
-            <Tab.Screen name="Dashboard" component={Home} />
-            <Tab.Screen name="Transactions" component={Transactions} />
+            <Tab.Screen
+                name="Dashboard"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View>
+                            <FontAwesomeIcon
+                                name="bar-chart-o"
+                                size={24}
+                                color={
+                                    focused
+                                        ? theme.colors.primary
+                                        : theme.colors.lightGray_2
+                                }
+                            />
+                        </View>
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Post"
+                component={Transactions}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View>
+                            <AntDesignIcon
+                                name="plus"
+                                size={32}
+                                color={theme.colors.white}
+                            />
+                        </View>
+                    ),
+                    tabBarButton: (props) => <PostNewRecordTab {...props} />
+                }}
+            />
+            <Tab.Screen
+                name="Transactions"
+                component={Transactions}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View>
+                            <Ionicon
+                                name="person"
+                                size={24}
+                                color={
+                                    focused
+                                        ? theme.colors.primary
+                                        : theme.colors.lightGray_2
+                                }
+                            />
+                        </View>
+                    )
+                }}
+            />
         </Tab.Navigator>
     );
 }
