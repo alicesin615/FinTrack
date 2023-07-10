ada64491a28d496d17224783f1282d56075edb9d
alicesin615 <alicesin0615@gmail.com>
2023-07-11 06:47:14 +0800

    create user model

---
+++ backend/src/main/java/com/fintrack/model/User.java
@@ -0,0 +1,28 @@
+package com.fintrack.model.user;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document
+public class User {
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+}
