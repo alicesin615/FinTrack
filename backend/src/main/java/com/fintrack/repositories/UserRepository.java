69022ad259a51d8dfcf0d778f16b442bbcd80db6
alicesin615 <alicesin0615@gmail.com>
2023-07-17 00:15:37 +0800

    user findByEmail query

--- backend/src/main/java/com/fintrack/repositories/UserRepository.java
+++ backend/src/main/java/com/fintrack/repositories/UserRepository.java
@@ -3,8 +3,12 @@ package com.fintrack.repositories;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.fintrack.model.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<UserDetails> findByEmail(String username);
 
 }
