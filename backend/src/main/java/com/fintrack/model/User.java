2c0af2a2cda71e0fc20d2eea01112dfda38d299d
alicesin615 <alicesin0615@gmail.com>
2023-07-19 00:14:58 +0800

    update User class to implement UserDetails class

--- backend/src/main/java/com/fintrack/model/User.java
+++ backend/src/main/java/com/fintrack/model/User.java
@@ -1,12 +1,22 @@
 package com.fintrack.model;
 
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Document
-public class User {
+@Builder
+public class User implements UserDetails {
+
+    @Id
     private String id;
     private String username;
     private String email;
@@ -17,6 +27,31 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
