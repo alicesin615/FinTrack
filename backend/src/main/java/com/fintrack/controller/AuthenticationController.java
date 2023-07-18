c9f6507f9dfc289860fa84d2d40994e8f9ea7748
alicesin615 <alicesin0615@gmail.com>
2023-07-19 00:14:18 +0800

    register & authenticate post requests

---
+++ backend/src/main/java/com/fintrack/controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.fintrack.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fintrack.auth.AuthenticationRequest;
+import com.fintrack.auth.AuthenticationResponse;
+import com.fintrack.auth.RegisterRequest;
+import com.fintrack.services.AuthenticationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+        private final AuthenticationService authenticationService;
+
+        @PostMapping("/register")
+        public ResponseEntity<AuthenticationResponse> register(
+                        @RequestBody RegisterRequest request) {
+                return ResponseEntity.ok(authenticationService.register(request));
+
+        }
+
+        @PostMapping("/authenticate")
+        public ResponseEntity<AuthenticationResponse> authenticate(
+                        @RequestBody AuthenticationRequest request) {
+                return ResponseEntity.ok(authenticationService.authenticate(request));
+
+        }
+
+}
