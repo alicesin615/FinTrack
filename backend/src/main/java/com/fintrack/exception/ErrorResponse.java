525ad04adb4c81d8e9e818bdbe15f9aaf41359ca
alicesin615 <alicesin0615@gmail.com>
2023-07-22 01:36:56 +0800

    ErrorResponse class

---
+++ backend/src/main/java/com/fintrack/exception/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.fintrack.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private HttpStatus status;
+    private String message;
+    private String errorCode;
+    private LocalDateTime timestamp;
+}
