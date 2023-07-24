c2273037373249ec24f1dc0f2472767ef39d3040
alicesin615 <alicesin0615@gmail.com>
2023-07-25 00:18:37 +0800

    add unique index to user class

--- backend/src/main/java/com/fintrack/model/User.java
+++ backend/src/main/java/com/fintrack/model/User.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -25,6 +26,8 @@ public class User implements UserDetails {
     @Id
     private String id;
     private String username;
+
+    @Indexed(unique = true)
     private String email;
     private String password;
     private Date createdAt;
