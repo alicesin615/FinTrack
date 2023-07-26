0f3f3b1cce19eb111a4d6faa81d8e075c8f89446
alicesin615 <alicesin0615@gmail.com>
2023-07-27 00:05:48 +0800

    basic button component

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TouchableOpacityProps } from 'react-native';
+import styled from 'styled-components/native';
+
+interface ButtonProps extends TouchableOpacityProps {
+    styleType?: 'primary' | 'secondary';
+}
+
+const ButtonContainer = styled.TouchableOpacity<ButtonProps>`
+    background-color: ${(props) =>
+        props?.styleType === 'primary'
+            ? props.theme.colors.primary
+            : props.theme.colors.secondary};
+    padding: 16px 20px;
+    border-radius: 8px;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ButtonText = styled.Text<ButtonProps>`
+    font-size: 16px;
+    color: ${(props) =>
+        props.styleType === 'primary' ? '#ffffff' : '#007bff'};
+`;
+
+export const Button: React.FC<ButtonProps> = ({
+    styleType = 'primary',
+    children,
+    ...rest
+}) => {
+    return (
+        <ButtonContainer styleType={styleType} {...rest}>
+            <ButtonText styleType={styleType}>{children}</ButtonText>
+        </ButtonContainer>
+    );
+};
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,4 @@
 export { PrimaryText, CardTitleText } from './Text';
 export { Card } from './Card';
 export { Input } from './Input.tsx';
+export { Button } from './Button.tsx';
