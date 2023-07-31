4c4108eb631cda22ac82a1808b1d706f67ea5ca8
alicesin615 <alicesin0615@gmail.com>
2023-08-01 00:31:38 +0800

    new addition in colors

--- frontend/src/styles/colors.ts
+++ frontend/src/styles/colors.ts
@@ -6,5 +6,7 @@ export const colors = {
     lightGray1: '#64748b',
     lightGray2: '#94a3b8',
     lightGray3: '#e2e8f0',
-    white: '#ffffff'
+    white: '#ffffff',
+    lightGreen5: '#84cc16',
+    rose6: '#e11d48'
 };
