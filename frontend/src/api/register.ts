3c2b35fc1189291ef24b125865f6daef77450152
alicesin615 <alicesin0615@gmail.com>
2023-08-01 00:31:21 +0800

    typesafe register mutation

--- frontend/src/api/register.ts
+++ frontend/src/api/register.ts
@@ -1,12 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RegisterRequestParams } from '@models/apiRequest.model';
 
+interface RegisterResponse {
+    accessToken: string;
+    refreshToken: string;
+}
 export const register = createApi({
     reducerPath: 'register',
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:8080/api/v1'
     }),
     endpoints: (builder) => ({
-        register: builder.mutation({
+        register: builder.mutation<
+            RegisterResponse,
+            RegisterRequestParams | {}
+        >({
             query: (apiParams) => ({
                 url: '/auth/register',
                 method: 'POST',
