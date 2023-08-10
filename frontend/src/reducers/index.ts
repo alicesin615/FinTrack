810340679ee349f986f4b550192283792b33d1b8
alicesin615 <alicesin0615@gmail.com>
2023-08-11 01:18:27 +0800

    add user middleware to store

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -2,11 +2,13 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { register, login } from '@api/index';
+import { user } from '@api/user';
 
 const rootReducer = combineReducers({
     [register.reducerPath]: register.reducer,
     [login.reducerPath]: login.reducer,
-    form: formReducer
+    form: formReducer,
+    [user.reducerPath]: user.reducer
 });
 
 export default rootReducer;
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -2,12 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import rootReducer from './reducers';
 import { register, login } from '@api/index';
+import { user } from '@api/user';
 
 export const store = configureStore({
     reducer: rootReducer,
     devTools: process.env.NODE_ENV === 'development',
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({}).concat([register.middleware, login.middleware])
+        getDefaultMiddleware({}).concat([
+            register.middleware,
+            login.middleware,
+            user.middleware
+        ])
 });
 
 export type RootState = ReturnType<typeof store.getState>;
