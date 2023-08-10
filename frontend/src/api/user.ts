a51a8cbf46390a66c1ad00c972e97043dbaba3e1
alicesin615 <alicesin0615@gmail.com>
2023-08-10 22:09:20 +0800

    create query for fetching logged in user

---
+++ frontend/src/api/user.ts
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { User } from '@models/user.model';
+
+const prepareHeaders = (headers: Headers, { getState }) => {
+    // Get the access token from the Redux store
+    const state = getState();
+    const accessToken = state.login?.data?.accessToken;
+    console.log('access token in prepare headers', state.login);
+
+    // Add the access token to the Authorization header
+    headers.set('Authorization', `Bearer ${accessToken}`);
+};
+
+export const user = createApi({
+    reducerPath: 'user',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:8080/api/v1',
+        prepareHeaders
+    }),
+
+    endpoints: (builder) => ({
+        getLoggedinUser: builder.query<User, void>({
+            query: () => ({
+                url: '/user',
+                method: 'GET'
+            })
+        })
+    })
+});
+
+export const { useGetLoggedinUserQuery } = user;
